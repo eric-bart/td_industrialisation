992e7e7940970023fc46cd7d923f664fe726c351
ERIC-PC-PORTAB\barte <bart.eric@hotmail.com>
2024-03-18 17:17:50 +0100

    Dev: CMED & EBAR Description: Correction comportement test unitaire

--- src/test/java/tp04/metier/ActionSimpleTest.java
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -57,7 +57,7 @@ public class ActionSimpleTest {
 
         portefeuille.vendre(action_simple, 2);
         Assertions.assertEquals(portefeuille.mapActions.get(action_simple), 3);
-        portefeuille.vendre(action_simple, 3);
+        portefeuille.vendre(action_simple, 2);
         Assertions.assertThrows(IllegalArgumentException.class,() -> portefeuille.vendre(action_simple, 100));
         
         portefeuille.acheter(action_simple, 5);
