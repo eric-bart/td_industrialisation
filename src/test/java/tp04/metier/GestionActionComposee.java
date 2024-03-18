1e3934b3f633326f161f2fd89effaff6b82089c7
LothaireBly <lothaire.bailly1@gmail.com>
2024-03-18 16:39:50 +0100

    Dev :  Gauthier Laluyaux Review : Mortadha Boubaker & Lothaire Bailly Description : Tests vente actions composées

--- src/test/java/tp04/metier/GestionActionComposee.java
+++ src/test/java/tp04/metier/GestionActionComposee.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 public class GestionActionComposee {
      
     @Test
-    public void testAcheterActionComposee() {
+    void testAcheterActionComposee() {
         //Arrange
         ActionSimple action_simple = new ActionSimple("Action Test");
         ActionSimple action_simple_2 = new ActionSimple("Action Test_2");
@@ -45,5 +45,14 @@ public class GestionActionComposee {
         Assertions.assertEquals(portefeuille.mapActions.get(action_composee), 7);
     
         
+    }
+    
+    @Test
+    void testVendreActionComposee(){
+        ActionComposee action_composee = new ActionComposee("Action composee Test");
+        Portefeuille portefeuille = new Portefeuille();
+        portefeuille.acheter(action_composee, 2);
+        portefeuille.vendre(action_composee, 1);
+        Assertions.assertEquals(1, portefeuille.mapActions.get(action_composee));
     }
 }
